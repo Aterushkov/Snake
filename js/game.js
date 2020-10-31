55a940aa3ef45723751fac4e4878fd8331d7ac57
Dom <Sanek.s88@gmail.com>
2020-10-31 16:53:38 +0300

    31.10/2

--- js/game.js
+++ js/game.js
@@ -1,20 +1,37 @@
 let game ={
-    x:0,
-    y:0,
-    start: function (){
-        let canvas= document.getElementById("mycanvas");
-        let ctx = canvas.getContext("2d");
-
-        let background =new Image();
-        background.src = "img/background.png";
-        let cell =new Image();
-        cell.src = "img/cell.png";
-        background.addEventListener("load", ()=>{
-            window.requestAnimationFrame(()=>{
-                ctx.drawImage(background,this.x ,this.y);
-            });
+    canvas:null,
+    ctx:null,
+    sprites:{
+        background:null,
+        cell:null,
+    },
+    start(){
+        this.canvas = document.getElementById("mycanvas");
+        this.ctx =  this.canvas.getContext("2d");
+        this.preload(()=>{
+            this.run();
         });
-
+    },
+    preload(callback){
+        let loaded = 0;
+        let required = 2;
+        let onAssetLoad = ()=>{
+            ++loaded;
+            if(loaded>= required){
+                callback();
+            }
+        };
+        for(let key in this.sprites){
+            this.sprites[key] =new Image();
+            this.sprites[key].src = "img/"+key+".png";
+            this.sprites[key].addEventListener("load",onAssetLoad);
+        }
+    },
+    run(){
+        window.requestAnimationFrame(()=>{
+            this.ctx.drawImage(this.sprites.background,0 ,0);
+            this.ctx.drawImage(this.sprites.cell,320 ,180);
+        });
     }
 };
 game.start();
